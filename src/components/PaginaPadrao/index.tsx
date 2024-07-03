c266be536fbb66fdbc45a6fe5e99d7c3dc1d911f
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2024-07-03 19:11:08 -0300

    Fix: section logic

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -1,16 +1,33 @@
 import Section from '../section/Section';
 import Header from '../header/Header';
 import {Outlet} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useAuthContext } from '../../auth/auth';
+import { getUserLocalStorage } from '../../auth/util';
+import { IToken } from '../../interfaces/interfaces';
 
 export default function PaginaPadrao ({children}: {children?: React.ReactNode}){
-    return(
+  const [userAuthenticate, setUserAuthenticate] = useState<boolean>(false);
+  const user: IToken | null = getUserLocalStorage();
+  const providerUtils = useAuthContext();
+  useEffect(() => {
+    providerUtils
+      .verifyUser()
+      .then((res) => {
+        setUserAuthenticate(res)
+      })
+      .catch((err) => setUserAuthenticate(err));
+    
+  }, [userAuthenticate, providerUtils]);
+
+    return (
       <>
-        <Header/>
-        <Section/>
+        <Header />
+        <Section user={user} userAuthenticate={userAuthenticate} />
         <>
-          <Outlet/>
+          <Outlet />
           {children}
         </>
       </>
-    )
+    );
 }
