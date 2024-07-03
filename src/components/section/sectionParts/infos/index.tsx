7cfd19863c495badfdedc3766e72f7a8fceaa736
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2024-07-03 19:23:24 -0300

    Feat: section infos

---
+++ src/components/section/sectionParts/infos/index.tsx
@@ -0,0 +1,32 @@
+import {
+  ContainerListSection,
+  ContainerPopUp,
+  SectionMenu,
+  SectionUl,
+  SpanSection,
+} from "../../styles/components";
+
+export default function SectionInfos() {
+  return (
+    <SectionUl className="flex justify-between">
+      <li className="mx-4 flex items-center">
+        <PhoneIcon style={{ color: "red" }} />
+        <SpanSection className="ml-2 self-center text-sm">
+          (22)99942-2502
+        </SpanSection>
+      </li>
+      <li className="mx-4 flex items-center">
+        <EmailIcon style={{ color: "red" }} />
+        <SpanSection className="ml-2 self-center text-sm">
+          ericklucas@id.uff.br
+        </SpanSection>
+      </li>
+      <li className="mx-4 flex items-center">
+        <GitHubIcon style={{ color: "red", height: "20px" }} />
+        <SpanSection className="ml-2 flex self-center text-sm">
+          <a href="https://github.com/erickelc">Github</a>
+        </SpanSection>
+      </li>
+    </SectionUl>
+  );
+}
