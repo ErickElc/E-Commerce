7629e2752c004468e98bd27380eb85d8de886c97
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2024-07-03 19:10:36 -0300

    Feat: auth methods

--- src/auth/util.ts
+++ src/auth/util.ts
@@ -3,24 +3,51 @@ import { IToken } from "../interfaces/interfaces";
 export const setUserLocalStorage = (user: IToken| null) =>{
     localStorage.setItem('u', JSON.stringify(user));
 }
-export const getUserLocalStorage = () => {
-    const json = localStorage.getItem('u')
-    if(!json) return null;
-    const user = JSON.parse(json);
-    return user ?? null;
-}
+export const getUserLocalStorage = (): IToken | null => {
+  const json = localStorage.getItem("u");
+  if (!json) return null;
+  const user = JSON.parse(json);
+  return user ?? null;
+};
 export async function loginRequest(email: string, password: string){
     try {
         const request = await apiBack_End.post('api/users/login',{
             email: email,
             password: password
         })
-        const dataRequest = await apiBack_End.post('api/users/list/email',{
-            email: email
-        })
-        console.log(dataRequest)
-        return {data: request.data, status: request.status, outerData: dataRequest.data};
+        return {data: request.data.token, status: request.status, outerData: request.data};
     } catch (error) {
         return null;
     }
+}
+
+export async function  verifyToken(token: string){
+    try {
+        const request = await apiBack_End.get("auth/verify-token", {headers: {t: token}});
+        return request.status === 200 ? true : false;
+    } catch (error) {
+        return false;
+    } 
+}
+
+export async function verifyAdmin(token: string, userId: string) {
+  try {
+    const request = await apiBack_End.get(`auth/admin/${userId}`, {
+      headers: { t: token },
+    });
+    return request.status === 200 ? true : false;
+  } catch (error) {
+    return false;
+  }
+}
+
+export async function verifyUser(token: string, userId: string) {
+  try {
+    const request = await apiBack_End.get(`auth/on-access/${userId}`, {
+      headers: { t: token },
+    });
+    return request.status === 200 ? true : false;
+  } catch (error) {
+    return false;
+  }
 }
